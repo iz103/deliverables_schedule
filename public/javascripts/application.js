e0937ff1c0828b9b7a8f77bbc9f29bb86df547f9
Islam El-Bahnasy <Iz@Rachel-Hawkinss-MacBook.local>
2011-01-09 14:00:52 +0000

    fixed array in javascript for the deliveries cmulative graph by converting array to multidimensional array in javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function toMultiDimensionalArray(array, subarraysize) {
+	
+	var mlarray = [], a, b;
+	
+	for (a = 0, b = -1; a < array.length; a++) {
+		if (a % subarraysize === 0) {
+			b++;
+			mlarray[b] = [];
+		}
+		
+		mlarray[b].push(array[a]);
+	}
+	
+	return mlarray;
+}
+
+// based on http://stackoverflow.com/questions/4492385/how-to-convert-simple-array-into-two-dimensional-arraymatrix-in-javascript-or
