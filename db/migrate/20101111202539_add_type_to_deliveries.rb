754f54f4b290e36b3fd7254b9fa89f9126290239
Islam El-Bahnasy <Iz@Rachel-Hawkinss-MacBook.local>
2011-01-26 21:26:33 +0000

    adding files not added before

--- db/migrate/20101111202539_add_type_to_deliveries.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeToDeliveries < ActiveRecord::Migration
-  def self.up
-    add_column :deliveries, :type, :string
-  end
-
-  def self.down
-    remove_column :deliveries, :type
-  end
-end
